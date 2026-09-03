79a832d016d49b6fc7d1f2d5947d8e7054c769dc
juare <JUAREZJ0303@students.bowiestate.edu>
2026-09-03 09:58:48 -0400

    OC- 2

--- src/Student.java
+++ src/Student.java
@@ -7,12 +7,12 @@ public class Student {
 Student(){
     this.FN="No First Name";
     this.LN="No Last name";
-this.Sid=0;
+    this.Sid=0;
 }//three parameteres
     Student(String FN, String LN, int Sid){
-        this.FN=FN;
-        this.LN=LN;
-        this.Sid=Sid;
+    this.FN=FN;
+    this.LN=LN;
+    this.Sid=Sid;
 
     }
     public static void main() {
@@ -21,5 +21,21 @@ this.Sid=0;
 s1= new Student();
 System.out.println(s1);
 System.out.println(s1.FN);
+
+Student s2;
+s2= new Student("Tobiloba", "Ayodeji",14141);
+System.out.println(s2.FN);
+System.out.println(s2.LN);
+System.out.println(s2.Sid);
+
+Student s3;
+String FN="Miguel";
+String LN="Bascarortega";
+int Sid=31415;
+s3= new Student(FN,LN,Sid);
+System.out.println(s3.FN);
+System.out.println(s3.LN);
+System.out.println(s3.Sid);
     }
+
 }
