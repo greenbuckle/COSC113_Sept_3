22eb991d04b18580c405f39659d22ed3799f246e
juare <JUAREZJ0303@students.bowiestate.edu>
2026-09-03 19:12:23 -0400

    LAB 2

--- src/Student.java
+++ src/Student.java
@@ -1,45 +1,36 @@
+//https://github.com/greenbuckle/COSC113_Sept_3.git
 
-public class Student {
+public class Student{
 String FN;
 String LN;
 int Sid;
-//0 parameters
+
 Student(){
 this.FN="No First Name";
 this.LN="No Last name";
-this.Sid=0;
-}//three parameteres
-Student(String FN, String LN, int Sid) {//oc
+this.Sid=0;}
+
+Student(String FN, String LN, int Sid) {
 this.FN = FN;
 this.LN = LN;
-this.Sid = Sid;
+this.Sid = Sid;}
+
+Student(String FN, String LN){
+this.FN=FN;
+this.LN=LN;
+
 }
-//Student(String FN, String LN) {//oc
-//            this.FN = FN;
-//            this.LN = LN;// Sid isnt decalred so the deafult would be given
-//        }
+Student (String FN,  int Sid, String LN){
+this(FN,LN);
 
-Student(String FN, String LN) {
-this.FN = FN;
-this.LN = LN;
 }
 
-
-//Methods= Getters and Setters
 public String getFN() {
 return FN;
 }
 
 public void setFN(String FN) {
 this.FN = FN;
-}
-
-public int getSid() {
-return Sid;
-}
-
-public void setSid(int sid) {
-Sid = sid;
 }
 
 public String getLN() {
@@ -50,44 +41,40 @@ public void setLN(String LN) {
 this.LN = LN;
 }
 
-//you can create overloaded constructors by varying the parameter number, by varying the order(the sequence in which they are ex:stud(FN,LN, Sid) then using this.FN then Sid, string integer string
+public int getSid() {
+return Sid;
+}
 
-public static void main(String[] args) {
-    Student s6=new Student ();
-    //setting names
-    s6.FN= "Melanie";
-    s6.setLN("Thomas");
-    s6.LN="Simpson";
+public void setSid(int sid) {
+Sid = sid;
+}
 
-Student s4=new Student("Helena","Payton");
-System.out.println(s4.FN);
-System.out.println(s4.LN);//OC
-System.out.println(s4.Sid);//OC
-//Variable s1 is of student type
-Student s1;
-s1= new Student();
-System.out.println(s1);
+public static void main (String[]args){
+
+Student s1= new Student("thomas","jefferson");
 System.out.println(s1.FN);
+System.out.println(s1.LN);
+System.out.println(s1.Sid);
 
-Student s2;
-s2= new Student("Tobiloba", "Ayodeji",14141);
+Student s2=new Student();
 System.out.println(s2.FN);
 System.out.println(s2.LN);
 System.out.println(s2.Sid);
 
-Student s3;
-String FN="Miguel";
-String LN="Bascarortega";
-int Sid=31415;
-s3= new Student(FN,LN,Sid);
+Student s3=new Student("Charles","Chaplin",800592);
 System.out.println(s3.FN);
 System.out.println(s3.LN);
 System.out.println(s3.Sid);
 
-Student s5;
-s5=new Student();
-
+Student s4=new Student();
+s4.FN="Juan";
+s4.setLN("Johnson");
+s4.setSid(55662);
+System.out.println(s4.getFN());
+System.out.println(s4.getLN());
+System.out.println(s4.getSid());
 
 }
-
 }
+
+
